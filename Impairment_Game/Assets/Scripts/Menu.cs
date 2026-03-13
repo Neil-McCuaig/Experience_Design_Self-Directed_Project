8c9e3926f48f5a3fb44e32a06224480382e82d75
Neil-McCuaig <116813845+Neil-McCuaig@users.noreply.github.com>
2026-03-13 13:46:49 -0400

    Random changes
    Gotta go home.

---
+++ Impairment_Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void OnPlayNearsightButtonPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnPlayFarsightButtonPressed()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void OnPlayHardHearingButtonPressed()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
