161d128306b62babf22b40cdd67721e3568171cf
Neil-McCuaig <116813845+Neil-McCuaig@users.noreply.github.com>
2026-03-16 21:04:40 -0400

    Minimal Progress
    Had to order groceries. Motivation low. I'll continue tomorrow.

--- Impairment_Game/Assets/Scripts/Player.cs
+++ Impairment_Game/Assets/Scripts/Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float movementSpeed = 5.0f;
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+        }
     }
 }
