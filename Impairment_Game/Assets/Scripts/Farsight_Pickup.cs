3a1ee8028b5cc0ae52657625568356e1c4a57171
Neil-McCuaig <116813845+Neil-McCuaig@users.noreply.github.com>
2026-04-13 10:32:00 -0400

    Working on scripts
    Beginning to work on Farsight scripts.

---
+++ Impairment_Game/Assets/Scripts/Farsight_Pickup.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farsight_Pickup : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask buyables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
+
+        if (Input.GetMouseButtonDown(0)) 
+        { 
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, buyables)) 
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
